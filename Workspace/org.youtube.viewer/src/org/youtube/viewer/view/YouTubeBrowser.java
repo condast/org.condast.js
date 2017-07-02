9cdb98f568ce0f3378e115db529bd1651f96edcc
Kees <info@condast.com>
2017-07-02 17:08:11 +0200

    youtube browser added to maven bulid

--- Workspace/org.youtube.viewer/src/org/youtube/viewer/view/YouTubeBrowser.java
+++ Workspace/org.youtube.viewer/src/org/youtube/viewer/view/YouTubeBrowser.java
@@ -56,7 +56,6 @@ public class YouTubeBrowser extends Browser {
 		this.browser = this;
 		this.browser.addProgressListener(listener);
 		this.controller = new CommandController(this);
-		this.browser.setLayoutData( new GridData( SWT.FILL, SWT.FILL, true, true));
 		listeners = new ArrayList<IEvaluationListener<Map<String, String>>>();
 		session.init(parent.getDisplay());
 		session.start();
