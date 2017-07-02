5a28a2d753e82afffae19271cbfd0ad16617dd54
Kees <info@condast.com>
2017-07-02 23:49:10 +0200

    OpenLayer controller works accvording to new strategy

--- Workspace/org.openlayer.map/src/org/openlayer/map/controller/OpenLayerController.java
+++ Workspace/org.openlayer.map/src/org/openlayer/map/controller/OpenLayerController.java
@@ -7,14 +7,15 @@ import org.eclipse.swt.browser.Browser;
 
 public class OpenLayerController extends AbstractJavascriptController{
 
-	public static final String S_INDEX_HTML = "/openlayer/index.html";
+	public static final String S_INDEX_HTML = "/resources/index.html";
 	public static final String S_INITIALISTED_ID = "OpenLayerInitialisedId";
 	public static final String S_IS_INITIALISTED = "isInitialised";
 	
 	private Logger logger = Logger.getLogger( this.getClass().getName() );
 
 	public OpenLayerController( Browser browser ) {
-		super( browser, S_INITIALISTED_ID, S_INDEX_HTML );
+		super( browser, S_INITIALISTED_ID );
+		setBrowser(OpenLayerController.class.getResourceAsStream( S_INDEX_HTML ));
 	}
 
 	@Override
