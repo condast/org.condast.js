cf71eebaae8d814563a82fae910d29e52ba32a08
Condast <info@condast.com>
2018-07-12 21:47:11 +0200

    Basic application works in JEE mode

--- Workspace/test.openlayers.map/src/test/openlayers/map/BasicApplication.java
+++ Workspace/test.openlayers.map/src/test/openlayers/map/BasicApplication.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.eclipse.rap.rwt.application.Application;
+import org.eclipse.rap.rwt.application.Application.OperationMode;
 import org.eclipse.rap.rwt.application.ApplicationConfiguration;
 import org.eclipse.rap.rwt.client.WebClient;
 
@@ -13,6 +14,7 @@ public class BasicApplication implements ApplicationConfiguration {
     public void configure(Application application) {
         Map<String, String> properties = new HashMap<String, String>();
         properties.put(WebClient.PAGE_TITLE, "Hello RAP");
+        application.setOperationMode( OperationMode.SWT_COMPATIBILITY );       
         application.addEntryPoint("/home", BasicEntryPoint.class, properties);
     }
 
