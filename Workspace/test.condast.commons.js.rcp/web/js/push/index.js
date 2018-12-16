13913e52d56e649d1c1161828c6e8eed3d5bea68
Condast <info@condast.com>
2018-12-17 00:01:11 +0100

    Improvements in index.js

--- Workspace/test.condast.commons.js.rcp/web/js/push/index.js
+++ Workspace/test.condast.commons.js.rcp/web/js/push/index.js
@@ -1,6 +1,53 @@
 const result = checkPush();
 
+var subscription;
+
+function go(){
+	registerServiceWorker();
+}
+
+function registerServiceWorker() { 
+	return navigator.serviceWorker.register('/test/js/push/collin-service.js') .then( 
+		function( registration) { 
+				console.log(' Service worker successfully registered.'); 
+				return registration; }
+		).catch( function( err) { 
+				console.error(' Unable to register service worker.', err);
+		});
+}
+
+function askPermission() { 
+	return new Promise( function( resolve, reject) { 
+				const permissionResult = Notification.requestPermission( 
+					function( result) { 
+						resolve( result); }); 
+					if (permissionResult) { 
+						permissionResult.then( resolve, reject); 
+					}
+			}).then( function( permissionResult){ 
+					if (permissionResult !== 'granted') { 
+						throw new Error(' We weren\'t granted permission.'); 
+					} 
+	}); 
+}
+
+function setButton(){
+	console.log("register button");
+	document.getElementById('doIt').onclick = function() {
+		const delay = document.getElementById('notification-delay').value;
+		const ttl = document.getElementById('notification-ttl').value;
+		console.log('Requesting notification');
+		fetch('http://localhost:10080/moodle/module/advice?id=1&token=12&module-id=1&activity-id=1', {
+			method: 'get',
+			headers: {
+				'Content-type': 'application/json'
+			},
+		});
+	};
+}
+
 function checkPush(){
+	setButton();
 	if (!('serviceWorker' in navigator)) {
 		console.log("Not in navigator");
 		return false;
@@ -27,8 +74,7 @@ function checkPush(){
 				}
 
 				//Get the server’s public key
-				const response = await fetch('./vapidPublicKey');
-				const vapidPublicKey = await response.text();
+				const vapidPublicKey = 'BDvq04Lz9f7WBugyNHW2kdgFI7cjd65fzfFRpNdRpa9zWvi4yAD8nAvgb8c8PpRXdtgUqqZDG7KbamEgxotOcaA';
 
 				//Chrome doesn’t accept the base64-encoded (string) vapidPublicKey yet urlBase64ToUint8Array() is defined in /tools.js
 				const convertedVapidKey = urlBase64ToUint8Array(vapidPublicKey);
@@ -43,7 +89,7 @@ function checkPush(){
 
 			console.log("fetch button");
 			//Send the subscription details to the server using the Fetch API.
-			fetch('./register', {
+			fetch('http://localhost:10080/moodle/module/advice?id=1&token=12&', {
 				method: 'post',
 				headers: {
 					'Content-type': 'application/json'
@@ -52,26 +98,8 @@ function checkPush(){
 					subscription: subscription
 				}),
 			});	
-			
-			console.log("register button");
-			document.getElementById('doIt').onclick = function() {
-				const delay = document.getElementById('notification-delay').value;
-				const ttl = document.getElementById('notification-ttl').value;
-				console.log('Requesting notification');
-				fetch('./sendNotification', {
-					method: 'post',
-					headers: {
-						'Content-type': 'application/json'
-					},
-					body: JSON.stringify({
-						subscription: subscription,
-						delay: delay,
-						ttl: ttl,
-					}),
-				});
-			};
 
-		});
+		});	
 	});
 	return true;
 }
