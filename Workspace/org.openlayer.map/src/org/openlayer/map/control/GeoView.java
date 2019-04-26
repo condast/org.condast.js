631194652fc256dab45a4098cec08f14a5957406
Kees Pieters <info@condast.com>
2019-04-27 00:51:52 +0200

    Minor improvements in geoview

--- Workspace/org.openlayer.map/src/org/openlayer/map/control/GeoView.java
+++ Workspace/org.openlayer.map/src/org/openlayer/map/control/GeoView.java
@@ -1,5 +1,6 @@
 package org.openlayer.map.control;
 
+import org.condast.commons.data.latlng.FieldData;
 import org.condast.commons.data.latlng.LatLng;
 import org.condast.js.commons.controller.IJavascriptController;
 
@@ -10,8 +11,7 @@ public class GeoView {
 	public static final float DEF_HORIZONTAL = 0.001f;
 	public static final float DEF_VERTICAL = 0.001f;
 	
-	private LatLng latlng;
-	private int zoom;
+	private FieldData fieldData;
 	
 	private IJavascriptController controller;
 
@@ -19,84 +19,100 @@ public class GeoView {
 		this( controller, null );
 	}
 	
-	public GeoView( IJavascriptController controller, LatLng latlng) {
+	public GeoView( IJavascriptController controller, FieldData fieldData) {
 		super();
 		this.controller = controller;
-		zoom = DEF_ZOOM;
-		this.latlng = latlng;	
+		this.fieldData = fieldData;	
 	}
 	
+	public FieldData getFieldData() {
+		return fieldData;
+	}
+
+	public void setFieldData(FieldData fieldData) {
+		this.fieldData = fieldData;
+	}
+
 	public LatLng getLatLng() {
-		return latlng;
-	}
-	
-	public int getZoom() {
-		return zoom;
-	}
-	public void setZoom(int zoom) {
-		this.zoom = zoom;
+		return this.fieldData.getCoordinates();
 	}
 	
 	public void setLongitude( double longitude ){
-		this.latlng = new LatLng( latlng.getLatitude(), longitude );
+		LatLng location = new LatLng( fieldData.getLatitude(), longitude );
+		this.fieldData.setCoordinates(location);
 		jump();		
 	}
 
 	public void setLatitude( double latitude ){
-		this.latlng = new LatLng( latitude, latlng.getLongitude() );
+		LatLng location = new LatLng( latitude, fieldData.getLongitude() );
+		this.fieldData.setCoordinates(location);
 		jump();		
 	}
 
 	public void setLatlng(LatLng latlng) {
-		this.latlng = latlng;
+		this.fieldData.setCoordinates( latlng );
 	}
 
+	public int getZoom() {
+		return this.fieldData.getZoom();
+	}
+	public void setZoom(int zoom) {
+		this.fieldData.setZoom(zoom);
+	}
+	
 	public String up(){
+		LatLng latlng = this.fieldData.getCoordinates();
 		double latitude = latlng.getLatitude() + DEF_VERTICAL;
-		this.latlng = new LatLng( latitude, latlng.getLongitude() );
+		this.setLatlng( new LatLng( latitude, latlng.getLongitude() ));
 		return jump();
 	}
 	
 	public String down(){
+		LatLng latlng = this.fieldData.getCoordinates();
 		double latitude = latlng.getLatitude() - DEF_VERTICAL;
 		if( latitude < 0 )
 			latitude = 0;
-		this.latlng = new LatLng( latitude, latlng.getLongitude() );
+		this.setLatlng( new LatLng( latitude, latlng.getLongitude() ));
 		return jump();
 	}
 
 	public String left(){
+		LatLng latlng = this.fieldData.getCoordinates();
 		double longitude = latlng.getLongitude() - DEF_HORIZONTAL;
 		if( longitude < 0 )
 			longitude = 0;
-		this.latlng = new LatLng( latlng.getLatitude(), longitude );
+		this.setLatlng( new LatLng( latlng.getLatitude(), longitude ));
 		return jump();
 	}
 
 	public String right(){
+		LatLng latlng = this.fieldData.getCoordinates();
 		double longitude = latlng.getLongitude() + DEF_HORIZONTAL;
-		this.latlng = new LatLng( latlng.getLatitude(), longitude );
+		this.setLatlng( new LatLng( latlng.getLatitude(), longitude ));
 		return jump();
 	}
 
 	public String zoom(){
 		String[] params = new String[1];
-		params[0] = String.valueOf( this.zoom );
+		params[0] = String.valueOf( this.fieldData.getZoom() );
 		String query = "zoom";
 		controller.setQuery( query, params );
 		return query;
 	}
 
 	public String zoomin() {
-		this.zoom ++;
+		int zoom = this.fieldData.getZoom();
+		this.fieldData.setZoom(zoom++);
 		String query = "zoomout";
 		controller.setQuery( query );
 		return query;
 	}
 
 	public String zoomout() {
-		if( this.zoom > 0)
-			this.zoom--;
+		int zoom = this.fieldData.getZoom();
+		if( zoom > 0)
+			zoom--;
+		this.fieldData.setZoom(zoom++);
 		String query = "zoomin";
 		controller.setQuery( query );
 		return query;
@@ -108,9 +124,10 @@ public class GeoView {
 
 	public String jump(){
 		String[] params = new String[3];
-		params[0] = String.valueOf( this.latlng.getLatitude() );
-		params[1] = String.valueOf( this.latlng.getLongitude());
-		params[2] = String.valueOf( this.zoom );
+		LatLng latlng = this.fieldData.getCoordinates();
+		params[0] = String.valueOf( latlng.getLatitude() );
+		params[1] = String.valueOf( latlng.getLongitude());
+		params[2] = String.valueOf( this.fieldData.getZoom());
 		String query = "jump";
 		controller.setQuery( query, params );
 		return query;
