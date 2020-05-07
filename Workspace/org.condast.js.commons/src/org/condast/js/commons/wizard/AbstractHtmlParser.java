309ec87b8779a7fb3f57a3c6d622cf4d5281b951
Kees Pieters <info@condast.com>
2020-05-07 17:46:26 +0200

    Additions to abstract html parser

--- Workspace/org.condast.js.commons/src/org/condast/js/commons/wizard/AbstractHtmlParser.java
+++ Workspace/org.condast.js.commons/src/org/condast/js/commons/wizard/AbstractHtmlParser.java
@@ -3,14 +3,29 @@ package org.condast.js.commons.wizard;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.eclipse.swt.browser.Browser;
 import org.eclipse.swt.browser.BrowserFunction;
 
-public abstract class AbstractHtmlWizard {
+public abstract class AbstractHtmlParser {
+
+	public enum Resources{
+		HEADER,
+		NAVIGATION,
+		INDEX,
+		FOOTER;
+
+		@Override
+		public String toString() {
+			return super.toString();
+		}
+		
+		public String toFile() {
+			return "/resources/" + super.toString().toLowerCase() + ".ht";
+		}
+	}
 
 	public static final String S_HEADER = "<!DOCTYPE html><html ";
 	public static final String S_LANGUAGE = "lang='";
@@ -18,22 +33,38 @@ public abstract class AbstractHtmlWizard {
 
 	public static final String S_DISPOSED = "<html><h1><b>This page is Disposed</b><h1></html>";
 
+	public static final String S_RESOURCES = "/resources/";
+
 	public static final String REGEX = "\\$\\{(.+?)\\}";
 
 	public enum Functions{
 		LINK,
 		LABEL,
-		VALUE;
+		VALUE,
+		AUTHENTICATION,
+		SCRIPT;
 
 		@Override
 		public String toString() {
 			return super.toString().toLowerCase();
 		}	
+	}
+
+	public enum Authentication{
+		ID,
+		TOKEN,
+		IDENTIFIER;
+
+		@Override
+		public String toString() {
+			return super.toString().toLowerCase();
+		}		
 	}
 
 	public enum Attributes{
 		MIN,
-		MAX;
+		MAX,
+		AUTHENTICATION;
 
 		@Override
 		public String toString() {
@@ -41,27 +72,13 @@ public abstract class AbstractHtmlWizard {
 		}	
 	}
 
-	private String header;
-	private String footer;
 	private Browser browser;
+	private Class<?> clss;
 
-	public AbstractHtmlWizard( Browser browser ) {
-		this( browser, Locale.ENGLISH);
-	}
-
-	public AbstractHtmlWizard( Browser browser, Locale locale ) {
-		this( browser, locale.getLanguage());
-	}
-
-	public AbstractHtmlWizard( Browser browser, String language ) {
-		this( browser, S_HEADER + S_LANGUAGE + language + "'>", S_FOOTER);
-	}
-
-	public AbstractHtmlWizard( Browser browser, String header, String footer ) {
+	public AbstractHtmlParser( Browser browser, Class<?> clss ) {
 		super();
-		this.header = header;
-		this.footer = footer;
 		this.browser = browser;
+		this.clss = clss;
 		new BrowserFunction(browser, Functions.LINK.toString()) {
 
 			@Override
@@ -76,10 +93,17 @@ public abstract class AbstractHtmlWizard {
 
 	protected abstract String onHandleLabel( String id, Attributes attr );
 
+	protected abstract String onHandleAuthentication( String id, Authentication attr );
+
 	protected abstract String onHandleValues( Functions function, String id, Attributes attr );
 
-	public void createHeader( InputStream in ) throws IOException {
-		header = parse( in );
+	protected abstract String onHandleFunction( Functions function, String id, Attributes attr );
+
+	protected abstract String onHandleScript( Class<?> clss, String path );
+
+	public String create( Resources resource, Class<?> clss ) throws IOException {
+		InputStream in = clss.getResourceAsStream(resource.toFile());
+		return (in == null )? "":parse( in);
 	}
 
 	public String parse( InputStream in ) throws IOException {
@@ -94,8 +118,6 @@ public abstract class AbstractHtmlWizard {
 
 	protected String parse( String str ) throws IOException {
 		StringBuilder builder = new StringBuilder();
-		builder.append( header);
-		builder.append("\n");
         Pattern pattern = Pattern.compile( REGEX );
         Matcher matcher = pattern.matcher(str);
 
@@ -110,6 +132,15 @@ public abstract class AbstractHtmlWizard {
         		String replacement = "javascript:" + split[0] + "(\"" + split[1] + "\")";
         		builder.append(replacement);
         		break;
+        	case SCRIPT:
+        		String path = (split.length == 2 )? split[1]: split[1] + "/" + split[2]; 
+        		path = S_RESOURCES + path.replace("^", ".");
+         		builder.append( onHandleScript( clss, path ));
+        		break;
+        	case AUTHENTICATION:
+        		Authentication auth = Authentication.valueOf(split[2].toUpperCase());
+        		builder.append( onHandleAuthentication(split[1], auth ));
+        		break;
         	case LABEL:
         		attr = Attributes.valueOf(split[2].toUpperCase());
         		builder.append( onHandleLabel(split[1], attr ));
@@ -119,25 +150,43 @@ public abstract class AbstractHtmlWizard {
         		builder.append( onHandleValues(function, split[1], attr ));
         		break;
         	default:
+        		attr = Attributes.valueOf(split[2].toUpperCase());
+        		builder.append( onHandleFunction(function, split[1], attr ));
         		break;
         	}
         	i = matcher.end();
         }
         builder.append(str.subSequence(i, str.length()));
-        builder.append("\n");
-        builder.append( footer);
 		return builder.toString();	
 	}
 
-	public void createPage( String str ) throws IOException {
-		this.browser.setText( str );
-		this.browser.requestLayout();	
+	public void createMainPage( Resources resource ) {
+		createPage(clss.getResourceAsStream( resource.toFile() ));
 	}
 
-	public void createPage( InputStream in ) throws IOException {
-		createPage( parse( in ));
+	public void createMainPage( ) {
+		this.createMainPage(Resources.INDEX);
 	}
 
+	public void createPage( InputStream in ) {
+		try {
+			StringBuilder builder = new StringBuilder();
+			builder.append( create(Resources.HEADER, clss));
+			builder.append("\n");
+			builder.append("<body onload='load()'>\n");
+			builder.append( create(Resources.NAVIGATION, clss));
+			builder.append( parse( in ));
+			builder.append("\n");
+	        builder.append( create(Resources.FOOTER, clss));
+	        builder.append("</body></html>");
+			this.browser.setText( builder.toString() );
+			this.browser.requestLayout();	
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	public void setVisible(boolean visible) {
 		this.browser.setVisible(visible);
 	}
