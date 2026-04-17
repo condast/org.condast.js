edbe5cb3a1c7e5337b68d7f51065177dba60b64b
condast <info@condast.com>
2026-04-17 18:33:16 +0200

    Trying to activate mapfilter

--- Workspace/org.openlayer.map/src/org/openlayer/map/servlet/MapFilter.java
+++ Workspace/org.openlayer.map/src/org/openlayer/map/servlet/MapFilter.java
@@ -3,21 +3,24 @@ package org.openlayer.map.servlet;
 import java.io.IOException;
 import java.util.logging.Logger;
 
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.ServiceScope;
 import org.osgi.service.http.whiteboard.propertytypes.HttpWhiteboardFilterPattern;
 
 @Component(scope=ServiceScope.PROTOTYPE,
-property= "osgi.http.whiteboard.filter.regex=/openlayer/*")
-@HttpWhiteboardFilterPattern( "/*")
+property= MapFilter.S_OSGI_FILTER_PATTERN)
+@HttpWhiteboardFilterPattern( MapFilter.S_CONTEXT_PATH)
 public class MapFilter implements Filter {
+
+	public static final String S_CONTEXT_PATH = "/openlayer/*";
+	public static final String S_OSGI_FILTER_PATTERN = "osgi.http.whiteboard.filter.pattern=" + S_CONTEXT_PATH;
 
 	private Logger logger = Logger.getLogger( this.getClass().getName() );
 
